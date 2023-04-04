ff97513337c35144515c6e043be089cdb674ba5f
Deltavf <deltavoid82@gmail.com>
2023-04-04 19:53:08 +0700

    Membuat controller, model, migrasi dan skema table Volume

---
+++ database/migrations/2023_04_04_122438_create_volumes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('volumes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('novel_id');
+            $table->foreign('novel_id')->references('id')->on('novels')->onDelete('cascade');
+            $table->string('judul')->unique();
+            $table->string('slug')->unique();
+            $table->text('story');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('volumes');
+    }
+};
