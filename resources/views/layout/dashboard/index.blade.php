4b8e2da688801b74bde61b383262c09f01e188f5
Deltavf <deltavoid82@gmail.com>
2023-04-09 09:23:12 +0700

    Melakukan beberapa perubahan : 1. Memperbaiki icon di layout dashboard 2. Membuat data dammy akun user 3. Menghilangkan tombol remmeber me

--- resources/views/layout/dashboard/index.blade.php
+++ resources/views/layout/dashboard/index.blade.php
@@ -61,11 +61,10 @@
             <span class="nav-link-text ms-1">Genre</span>
           </a>
         </li>
-        @endif
         <li class="nav-item">
-          <a class="nav-link @if(Request::is('dashboard/user*')) active @endif" href="/dashboard/user">
+          <a class="nav-link @if(Request::is('dashboard/user*')) active @endif" href="/dashboard/genre">
             <div
-              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              class="icon icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -90,6 +89,7 @@
             <span class="nav-link-text ms-1">User</span>
           </a>
         </li>
+        @endif
         <li class="nav-item">
           <a class="nav-link" href="/">
             <div
